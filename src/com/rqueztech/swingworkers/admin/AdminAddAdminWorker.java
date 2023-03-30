d388a49cb9c0b7cf7efdd16d27199c6b18a247a6
Ricardo Quezada <77219945+rqueztech@users.noreply.github.com>
2023-03-30 17:56:02 +0900

    Swing Workers Created
    Optimized structure of admin swingworkers.

---
+++ src/com/rqueztech/swingworkers/admin/AdminAddAdminWorker.java
@@ -0,0 +1,69 @@
+package com.rqueztech.swingworkers.admin;
+
+import java.util.concurrent.ExecutionException;
+
+import javax.swing.JOptionPane;
+import javax.swing.SwingWorker;
+
+import com.rqueztech.encryption.PasswordEncryption;
+
+public class AdminAddAdminWorker extends SwingWorker<Boolean, Void> {
+	private String adminAccountName;
+	private String adminFirstName;
+	private String adminLastName;
+	private char[] newAdminPassword;
+	private byte[] newAdminSalt;
+	private String gender;
+	private int adminNumber;
+	
+	private PasswordEncryption passwordEncryption;
+	
+	// --------------------------------------------------------------------------------------
+	public AdminAddAdminWorker(String adminAccountName, String adminFirstName, String adminLastName, char[] newAdminPassword, String gender, int adminNumber) {
+		this.passwordEncryption = new PasswordEncryption();
+		
+		this.adminAccountName = adminAccountName;
+		this.adminFirstName = adminFirstName;
+		this.adminLastName = adminLastName;
+		this.newAdminPassword = newAdminPassword;
+		this.generateNewAdminSalt();
+		this.gender = gender;
+		this.adminNumber = adminNumber;
+	}
+	
+	// --------------------------------------------------------------------------------------
+	private void generateNewAdminSalt() {
+		this.newAdminSalt = passwordEncryption.generateSalt();
+	}
+	
+	// --------------------------------------------------------------------------------------
+	@Override
+	protected Boolean doInBackground() throws Exception {
+		// TODO Auto-generated method stub
+		
+		return true;
+	}
+
+	
+	@Override
+	protected void done() {
+		try {
+			boolean success = get();
+
+			if (success) {
+				String dataPassed = String
+						.format("%s, %s, %s, ",
+								this.adminAccountName,
+								this.adminFirstName,
+								this.adminLastName);
+				
+				JOptionPane.showMessageDialog(null, dataPassed);
+			} else {
+				JOptionPane.showMessageDialog(null, "AdminAddAdminWorker FAIL");
+			}
+		} catch (InterruptedException | ExecutionException e) {
+			JOptionPane.showMessageDialog(null, "Error searching error.", "Error", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+	}
+}
